ff72d857026b714285bea03bfcc31eff66d36b8c
Spencer Alderman <spam.mehere@hotmail.com>
2013-11-05 17:40:42 -0500

    Implement jquery / bootstrap

--- index.php
+++ index.php
@@ -12,12 +12,10 @@ function __autoload($class_name) {
         <title>Testing page</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="assets/css/main.css" rel="stylesheet" media="screen">
-        <link href="assets/css/menu.css" rel="stylesheet" media="screen">
-        <link href="assets/css/table.css" rel="stylesheet" media="screen">
-        <script src="bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery-1.10.2.min.js"></script>
     </head>
     <body>
         <div class="container">
